6886ade0aba995973443631183f9f92753af307f
sabari-35 <sabari.psmail@gmail.com>
2025-12-23 13:50:29 +0530

    server

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="VITE_GOOGLE_CLIENT_ID">
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+
       <BrowserRouter>
         <App />
       </BrowserRouter>
